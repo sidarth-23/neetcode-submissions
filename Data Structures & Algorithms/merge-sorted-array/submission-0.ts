a98594f14063b200a193f49e9635a92b0a28dc23
Sidarth G <58748190+sidarth-23@users.noreply.github.com>
2026-08-04 20:09:36 +0530

    Add: merge-sorted-array - submission-0

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-0.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number} m
+     * @param {number[]} nums2
+     * @param {number} n
+     * @return {void} Do not return anything, modify nums1 in-place instead.
+     */
+    merge(nums1: number[], m: number, nums2: number[], n: number): void {
+        let i = m - 1
+        let j = n - 1
+        let k = m + n -1
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--]
+            } else {
+                nums1[k--] = nums2[j--]
+            }
+        }
+    }
+}
