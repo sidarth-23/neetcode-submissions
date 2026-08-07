acf5dea1af5cbc09d480dd86cb5596dc7a53ee5e
Sidarth G <58748190+sidarth-23@users.noreply.github.com>
2026-08-07 16:30:13 +0530

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.ts
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    validPalindrome(s: string): boolean {
+        let l = 0, r = s.length - 1
+        while (l < r) {
+            if (s[l] !== s[r]) {
+                return (this.isPalindrome(s.slice(0,l) + s.slice(l + 1)) || this.isPalindrome(s.slice(0, r) + s.slice(r + 1)))
+            }
+            l++;
+            r--
+        }
+        return true
+    }
+
+    isPalindrome(s: string): boolean {
+        let l = 0, r = s.length - 1
+        while (l < r) {
+            if (s[l] !== s[r]) {
+                return false
+            }
+            r--;
+            l++;
+        }
+        return true
+    }
+}
