1d11a94452f94ecb427b41bb44b24e762647d69a
Sidarth G <58748190+sidarth-23@users.noreply.github.com>
2026-08-13 13:39:52 +0530

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {boolean}
+     */
+    containsNearbyDuplicate(nums: number[], k: number): boolean {
+        const set = new Set()
+        let l = 0
+        for (let r = 0; r < nums.length; r++) {
+            if (r - l > k) {
+                set.delete(nums[l])
+                l++
+            }
+            if (set.has(nums[r])) {
+                return true
+            }
+            set.add(nums[r])
+        }
+
+        return false
+    }
+}
